5f766fa83eb559d66cff7462e712d2fc09970893
2506lanlan <91785700+2506lanlan@users.noreply.github.com>
2023-11-06 22:26:35 +0700

    Add expressJS

--- backend/config.js
+++ backend/config.js
@@ -1,4 +1,4 @@
 export const PORT = 2727;
 
-export const mongoDBURL = 
-    'mongodb+srv://2506lanlan:25062003@itproject.a7zir3z.mongodb.net/bookstore?retryWrites=true&w=majority';
+export const mongoDBURL =
+  "mongodb+srv://2506lanlan:25062003@itproject.a7zir3z.mongodb.net/bookstore?retryWrites=true&w=majority";
--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,19 @@
 import express from "express";
-import {PORT, mongoDBURL} from "./config.js";
-
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose from "mongoose";
 const app = express();
 app.get("/", (req, res) => {
-    console.log(req);
-    return res.status(234).send('MERN bookstore');
-})
-app.listen(PORT, () => {
-    console.log(`Current port: ${PORT}`);
+  console.log(req);
+  return res.status(234).send("MERN bookstore");
 });
+mongoose
+  .connect(mongoDBURL)
+  .then(() => {
+    console.log("Connected to Database");
+    app.listen(PORT, () => {
+      console.log(`Current port: ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
